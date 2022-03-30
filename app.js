9bd2498dfef6a62240d56ccffac7e3463998c356
Ryle Edwards <ryle.edwards@gmail.com>
2022-03-30 15:15:44 -0500

    Implement grid resize from user

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 // Create 16x16 grid of square divs
 
 const mainContainer = document.getElementById("main-container");
+const btnClear = document.getElementById("btnClear");
+
+const height = 720;
+const width = 720;
+let dimension = 16;
+
+btnClear.addEventListener("click", () => {
+  dimension = promptUser();
+  removeAllChildNodes(mainContainer);
+  createSquares();
+});
 
 createSquares();
 
 function createSquares() {
   //create rows
-  for (i = 0; i < 16; i++) {
+  for (i = 0; i < dimension; i++) {
     let row = document.createElement("div");
     //create columns
-    for (j = 0; j < 16; j++) {
+    for (j = 0; j < dimension; j++) {
       let square = document.createElement("div");
       square.setAttribute("class", "square");
+      square.style.height = height / dimension + "px";
+      square.style.width = width / dimension + "px";
       row.appendChild(square);
-      console.log(`created column ${j}`);
     }
     row.setAttribute("class", "row");
     mainContainer.appendChild(row);
-    console.log(`created row ${i}`);
   }
 }
 
-// function createSquares() {
-//   for (i = 0; i < 256; i++) {
-//     let square = document.createElement("div");
-//     square.style.height = "16px";
-//     square.style.width = "16px";
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
-//     square.style.border = "solid black 1px";
-//     mainContainer.appendChild(square);
-//   }
-// }
+function promptUser() {
+  let newDimensions = parseInt(
+    prompt(
+      "How many squares per side would you like in your new grid? (Up to 100)",
+      "64"
+    )
+  );
+  while (newDimensions > 100) {
+    newDimensions = parseInt(prompt("Dimensions cannot exceed 100", "64"));
+  }
+  return newDimensions;
+}
